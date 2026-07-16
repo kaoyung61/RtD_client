11a3a2731cb836b5e1d7c56e94070cd5e56bbc59
A Jung <kaoyung61@gmail.com>
2026-07-16 23:20:06 +0200

    Initial client

---
+++ script.js
@@ -0,0 +1,25 @@
+const button = document.getElementById("send");
+
+button.onclick = async () => {
+
+    const value = document.getElementById("value").value;
+
+    const response = await fetch("https://rtd-b8p7.onrender.com/api/query", {
+
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+            value
+        })
+
+    });
+
+    const data = await response.json();
+
+    document.getElementById("answer").innerText = data.result;
+
+};
