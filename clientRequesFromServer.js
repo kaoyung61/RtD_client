ea69d2a31221b871f133a6d2d38fecb44e67fb5f
Alexander <kaoyung61@gmail.com>
2026-08-09 14:24:09 +0200

    cvnm

--- clientRequesFromServer.js
+++ clientRequesFromServer.js
@@ -4,6 +4,7 @@ export async function client_AntwortFromServer(data) {
     switch (data.command) {
         case "message":     {return     getMessage(data);}
         case "token":       {return     getToken(data);}
+        case "rooms":       {return     getRooms(data);}
         //case "tockenUpdate": {
         //    playerToken = data.token;
         //    localStorage.setItem("playerToken", playerToken);
@@ -21,8 +22,19 @@ export async function getMessage(data) {
 }
 
 export async function getToken(data) {
-    //sendToSocket(socket, { command: "token", token: player.token });
+    /* Server:
+                sendToSocket(socket, { command: "token", token: player.token });
+    */
 
     localStorage.setItem("token", data.token);
     console.log("Token UPD:", data.token);
+}
+
+export async function getRooms(data) {
+    /* Server:
+
+    */
+
+
+    console.log("Server rooms:", data.rooms);
 }
