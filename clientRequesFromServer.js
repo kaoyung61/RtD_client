59b77715f6e0ca74a56ec605d469717722caff01
Alexander <kaoyung61@gmail.com>
2026-08-09 14:01:35 +0200

    2026-08-09-1401_Token Save

--- clientRequesFromServer.js
+++ clientRequesFromServer.js
@@ -1,11 +1,9 @@
 //import { CR_authoriseMe, CR_loginClient, CR_newClientRegistration } from "./gameLogic/serverLoginRequest.js";
 
 export async function client_AntwortFromServer(data) {
-    switch (data.type) {
-        case "message": {
-            console.log("Server message:", data.text);
-            break;
-        }
+    switch (data.command) {
+        case "message":     {return     getMessage(data);}
+        case "token":       {return     getToken(data);}
         //case "tockenUpdate": {
         //    playerToken = data.token;
         //    localStorage.setItem("playerToken", playerToken);
@@ -16,4 +14,15 @@ export async function client_AntwortFromServer(data) {
             console.log("Server unnown event:", data);
     }
 
+}
+
+export async function getMessage(data) {
+    console.log("Server message:", data.text);
+}
+
+export async function getToken(data) {
+    //sendToSocket(socket, { command: "token", token: player.token });
+
+    localStorage.setItem("token", data.token);
+    console.log("Token UPD:", data.token);
 }
