781223dd5928cbd412868318dea3446e948ebd07
Alexander <kaoyung61@gmail.com>
2026-09-06 01:06:41 +0200

    kleine fehlern

--- clientMain.js
+++ clientMain.js
@@ -1,24 +1,9 @@
-console.log("version 05.09_2");
-
-import { connectToServer } from "./clientNetwork.js";
-
-import {
-    createStartScreen,
-    showLoginScreen
-} from "./ui/clientStartScreen.js";
-
-import {
-    createGameScreen,
-} from "./ui/clientGameScreen.js";
-
-//setTimeout(() => {showLoginScreen();}, 1500);
-//setTimeout(() => {showStartScreen();}, 1500);
-
-connectToServer();
+console.log("version 06.09_1");
 
 
-//createStartScreen();
-//createGameScreen();
+setTimeout(() => {connectToServer();}, 1500);
+
+
 
 
 
